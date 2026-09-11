7bc0925fac6d51efaae09a3a7837436e3a2f1caa
Kenny <aradb171@gmail.com>
2026-09-11 15:15:56 +0330

    designed profile for user

---
+++ config/apps/accounts/models/profiles.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile'
+    )
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    avatar = models.ImageField(default='avatars/default.jpg')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user
