36110b63bd7167edf64d3eb524dd9fb6ea3ad3c3
Kenny <aradb171@gmail.com>
2026-09-11 18:07:23 +0330

    added signals for user
    user creation -> create profile for user
    user deletion -> update user's post's author to a "deleted_user" record

--- config/apps/accounts/models/profiles.py
+++ config/apps/accounts/models/profiles.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='profile'
     )
-    first_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     avatar = models.ImageField(default='avatars/default.jpg')
     created_date = models.DateTimeField(auto_now_add=True)
---
+++ config/apps/accounts/signals.py
@@ -0,0 +1,28 @@
+from django.db.models.signals import post_save, pre_delete
+from django.dispatch import receiver
+from .models import Profile
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_profile_for_user(sender, instance, created, **kwargs):
+    if created and instance.email != settings.DELETED_USER_EMAIL:
+        Profile.objects.create(user=instance)
+
+
+@receiver(pre_delete, sender=User)
+def set_user_posts_author_to_deleted_user(sender, instance, **kwargs):
+    if instance.email != settings.DELETED_USER_EMAIL:
+        deleted_user, created = User.objects.get_or_create(email=settings.DELETED_USER_EMAIL)
+        if created:
+            deleted_user.set_unusable_password()
+            deleted_user.save(update_fields=['password'])
+        deleted_profile, _ = Profile.objects.get_or_create(
+            user=deleted_user,
+            defaults={'first_name': 'Deleted User'}
+        )
+        instance.profile.posts.update(author=deleted_profile)
