5b1046822531d289008ae19335d696721bb3e1ca
Kenny <aradb171@gmail.com>
2026-09-11 18:49:38 +0330

    designed comment model

--- config/apps/comment/admin.py
+++ config/apps/comment/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+    ordering = ('created_date',)
+    readonly_fields = ('created_date', 'updated_date')
+    list_display = ('user', 'id', 'post', 'created_date', 'updated_date')
+    search_fields = ('id', 'content')
+
+
+admin.site.register(Comment, CommentAdmin)
