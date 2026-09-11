186732afa855d45d18449b504e22a4ee9e5e83f8
Kenny <aradb171@gmail.com>
2026-09-11 13:49:27 +0330

    designed user model

---
+++ config/apps/accounts/models/users.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, UserManager as BaseUserManager, PermissionsMixin
+)
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('Email is required.'))
+        email = self.normalize_email(email)
+        user_obj = self.model(email=email, **extra_fields)
+        user_obj.set_password(password)
+        user_obj.save()
+        return user_obj
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True'))
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True'))
+        if extra_fields.get('is_active') is not True:
+            raise ValueError(_('Superuser must have is_active=True'))
+        return self.create_user(email=email, password=password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- config/config/settings/base.py
+++ config/config/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user model
+AUTH_USER_MODEL = 'accounts.User'
