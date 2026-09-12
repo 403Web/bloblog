c791ac60c3fa4afa936c44f598ed8d6b0398bfc6
Kenny <aradb171@gmail.com>
2026-09-12 22:26:09 +0330

    added a command to initialize the db with category objects

---
+++ config/apps/blog/management/commands/createcategories.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+
+from ...models import Category
+
+
+categories = [
+    'Technology', 'Programming', 'Science', 'Mathematics',
+    'Artificial Intelligence', 'Cybersecurity', 'Gadgets',
+    'Internet', 'Education', 'Books', 'Movies & TV',
+    'Music', 'Gaming', 'Cars', 'Travel', 'Lifestyle',
+    'Business', 'Finance', 'Psychology', 'Philosophy',
+    'History', 'News', 'Reviews', 'Tutorials', 'Personal',
+    'Other'
+]
+
+
+class Command(BaseCommand):
+    """
+    A command to initialize the database with the specified categories
+    in the list above. It will be used for build command right after
+    python manage.py migate.
+    """
+    help = 'Creates category objects and initializes the database.'
+
+    def handle(self, *args, **options):
+        cat_count = 0
+        for cat in categories:
+            if Category.objects.filter(name=cat).exists():
+                self.stdout.write(
+                    f'Category object "{self.style.SUCCESS(cat)}" already exists... '
+                    f'{self.style.WARNING("SKIPPING")}'
+                )
+                continue
+            self.stdout.write(
+                f'Creating category object "{self.style.WARNING(cat)}"...',
+                ending=''
+            )
+            Category.objects.create(name=cat)
+            self.stdout.write(self.style.SUCCESS(' DONE'))
+            cat_count += 1
+        if cat_count == 0:
+            self.stdout.write('No category object created.')
+        elif cat_count == 1:
+            self.stdout.write('1 category object has been created successfully.')
+        else:
+            self.stdout.write(
+                f'{cat_count} category objects have been created successfully.'
+            )
+        
