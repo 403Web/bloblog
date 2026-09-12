d9235af198df353695611579db3671dc78a54b2e
Kenny <aradb171@gmail.com>
2026-09-12 22:50:26 +0330

    added filters to posts endpoint

--- config/apps/blog/api/v1/views.py
+++ config/apps/blog/api/v1/views.py
@@ -1,5 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from .permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer
@@ -7,6 +9,12 @@ from ...models import Post
 
 
 class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    filter_backends = [
+        DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter
+    ]
+    filterset_fields = ['category', 'category__name']
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_date']
