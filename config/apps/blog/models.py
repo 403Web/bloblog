d109e09e84506d8bb2bd248720290f1299ded372
Kenny <aradb171@gmail.com>
2026-09-12 19:16:23 +0330

    modified category model

--- config/apps/blog/models.py
+++ config/apps/blog/models.py
@@ -47,7 +47,7 @@ class PostLike(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
