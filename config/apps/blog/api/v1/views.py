27b92d1bd4f73a3aa33477069cb2ca758381c494
Kenny <aradb171@gmail.com>
2026-09-15 17:44:49 +0330

    added endpoint for category

--- config/apps/blog/api/v1/views.py
+++ config/apps/blog/api/v1/views.py
@@ -3,10 +3,10 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
-from .serializers import PostSerializer
-from .permissions import IsOwnerOrReadOnly
+from .serializers import PostSerializer, CategorySerializer
+from .permissions import IsOwnerOrReadOnly, IsAdminOrReadOnly
 from .paginations import PostPagination
-from ...models import Post
+from ...models import Post, Category
 
 
 class PostViewSet(ModelViewSet):
@@ -20,3 +20,9 @@ class PostViewSet(ModelViewSet):
     filterset_fields = ['category', 'category__name']
     search_fields = ['title', 'content']
     ordering_fields = ['created_date']
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminOrReadOnly]
