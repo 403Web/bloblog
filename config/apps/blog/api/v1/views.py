2ea930c43c093d65290e21d03c302f30832bf1bb
Kenny <aradb171@gmail.com>
2026-09-15 17:32:58 +0330

    added pagination to blog_api_v1:post-list

--- config/apps/blog/api/v1/views.py
+++ config/apps/blog/api/v1/views.py
@@ -3,8 +3,9 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 
-from .permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
+from .paginations import PostPagination
 from ...models import Post
 
 
@@ -15,6 +16,7 @@ class PostViewSet(ModelViewSet):
     filter_backends = [
         DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter
     ]
+    pagination_class = PostPagination
     filterset_fields = ['category', 'category__name']
     search_fields = ['title', 'content']
     ordering_fields = ['created_date']
