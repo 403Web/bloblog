aa6fe745815d66b9acce45a0edfc657f376413ba
Kenny <aradb171@gmail.com>
2026-09-21 17:40:26 +0330

    updated templates

---
+++ config/static/scripts/comment.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("TUbtn");
+
+button.addEventListener("click", function () {
+    button.classList.toggle("text-light-emphasis");
+    button.classList.toggle("text-primary");
+});
